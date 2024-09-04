230ad54b1860fb9d3611efee121c3fdff90ab911
Shelly Ambar <42389001+ShellyAmbar@users.noreply.github.com>
2024-09-04 22:47:18 +0300

    adding option of multi types

--- screens/home/auto-complete-geo-location/hooks/useAutoCompleteGeoLocation.ts
+++ screens/home/auto-complete-geo-location/hooks/useAutoCompleteGeoLocation.ts
@@ -1,5 +1,4 @@
 import {useCallback, useEffect, useState} from 'react';
-import axios from 'axios';
 import useDebounce from './useDebounce';
 import UsePlaceAutocompleteProps from './interfaces';
 import {Keyboard} from 'react-native';
@@ -27,10 +26,10 @@ const useAutoCompleteGeoLocation = ({
       const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${input}&key=${quaryData.key}&types=${typesArray}`;
 
       try {
-        const response = await axios.get(url);
-
-        if (response.data.predictions) {
-          setSuggestions(response.data.predictions);
+        const response = await fetch(url);
+        const data = await response.json();
+        if (data.predictions) {
+          setSuggestions(data.predictions);
         } else {
           setSuggestions([]);
         }
